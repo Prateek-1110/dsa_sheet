327244370a98d7707ebb89d14ae2769385175a37
Prateek-1110 <b23bb1033@iitj.ac.in>
2025-05-31 10:42:33 +0530

    Today's ques

--- array/RotateBy90.cpp
+++ array/RotateBy90.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 #define vvi vector<vector<int>>
 // its best approach is transposing the matrix and then reversing each row
+/*
+[ 1 ,2 ,3                              1 4 7
+   4, 5 ,6        Its transpose is -   2 5 8 
+   7 , 8 , 9 ]                         3 6 9 
+*/
 void rotate(vvi &v){
     int n = v.size();
    for(int i = 0;i<n;i++){
