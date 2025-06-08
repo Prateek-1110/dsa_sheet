cbaf7a59389f0b15c17f58a10a7712136d374e5f
Prateek-1110 <b23bb1033@iitj.ac.in>
2025-06-08 12:49:43 +0530

    8 june

---
+++ LL/Medium/middle.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define cia(x) for(int i = 0; i < x.size(); i++) cin >> x[i];
+#define ll long long
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define srt(x) sort(x.begin(), x.end())
+#define rsrt(x) sort(x.rbegin(), x.rend())
+#define pb push_back
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode* middleNode(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+        while(slow->next!=nullptr && fast->next->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+}
